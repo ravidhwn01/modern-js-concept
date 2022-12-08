f459b30e01d57341c6428907e17b3c0bd236502f
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-09 02:16:27 +0530

    foreach method  and for of loop

--- forEach.js
+++ forEach.js
@@ -8,3 +8,15 @@ let arr = [1,3,5,7,9];
 arr.forEach((element,index,arr) => {
     console.log(element,index,arr);
 });
+
+// iterators in js (array and strings)
+// iterators are objects which have next() method
+// next() method returns an object with two properties
+// 1. value
+// 2. done
+// value is the next element in the sequence
+// done is true if the last element in the sequence has been reached otherwise false
+let monk = "this is my pet!"
+for(let item of monk){
+    console.log(item);
+}
