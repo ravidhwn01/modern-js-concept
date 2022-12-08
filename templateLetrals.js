a6eb487c730e40d7d85f362dfc801d1c5a26e0bf
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-09 02:34:16 +0530

    tamplate literals

---
+++ templateLetrals.js
@@ -0,0 +1,32 @@
+// template literals
+// template literals are string literals allowing embedded expressions
+// you can use multi-line strings and string interpolation features with them
+// they were called "template strings" in prior editions of the ES2015
+//
+// syntax
+// `string text`
+// `string text line 1
+// string text line 2`
+// `string text ${expression} string text`
+// `string text ${expression} string text`
+// tag `string text ${expression} string text`
+//
+// string interpolation
+"use strick"
+let monkey = `5 + 5 : ${10} `;
+console.log(monkey)
+
+// method of template literals
+// raw
+// raw is a property of the template literals
+// it returns a string of the raw string
+// it does not perform interpolation
+// it does not perform any escape sequences
+// it returns the string as it is
+// it is used to access the raw string
+// it is used to access the string as it is
+// it is used to access the string without any escape sequences
+// it is used to access the string without any interpolation
+// it is used to access the string without any special characters
+// syntax
+
