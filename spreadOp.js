902255e3d083f91592b4e233779ad74729dea1fe
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-13 11:22:41 +0530

    spread operator

--- spreadOp.js
+++ spreadOp.js
@@ -1,3 +1,4 @@
+"use strict"
 //spread operator
 //spread operator is used to split up array elements or object properties
 
@@ -9,3 +10,13 @@ const newObject = {...oldObject, newProp: 5};
 
 console.log(newArray);
 console.log(newObject)
+
+function demo([...para]){
+    // console.log(para);
+    let sum = 0;
+        for (let i = 0; i < para.length; i++) {
+            sum += para[i];
+        }
+        console.log(sum);
+}
+demo([1,2,3,4,5,6,7,8,9,10])
