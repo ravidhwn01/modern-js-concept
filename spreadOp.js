6e357198fc12a1ff41ee2521da1802342f83af7a
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-13 11:06:35 +0530

    spread operator

--- spreadOp.js
+++ spreadOp.js
@@ -1,2 +1,11 @@
 //spread operator
 //spread operator is used to split up array elements or object properties
+
+// examples
+const oldArray = [1, 2, 3];
+const oldObject = {prop: 1, prop2: 2};
+const newArray = [...oldArray, 1, 2];
+const newObject = {...oldObject, newProp: 5};
+
+console.log(newArray);
+console.log(newObject)
