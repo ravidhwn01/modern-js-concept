bc8e6e4d3d1d77fe3e980182bd43017f1d14c828
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-17 10:29:33 +0530

    async js and promises

--- async.js
+++ async.js
@@ -1,3 +1,4 @@
+
  function  ram(){
     return  new  Promise((resolve, reject) => {
         setTimeout(() => {
@@ -7,3 +8,36 @@
     });
  }
  ram();
+
+ //asynchronous programming in js
+    //Path: async.js
+    // A Promise is an object that keep track about whether
+    // a certain event has happened already or not.
+
+
+    // Promise states
+    // 1. Pending
+    // 2. Fulfilled
+    // 3. Rejected
+
+    // Promise is a constructor function
+    // Promise takes a function as an argument
+    // This function takes two parameters resolve and reject
+    // resolve and reject are also functions
+    const promiseObj  = new Promise((resolve,reject)=>{
+        // resolve();
+        setTimeout(() => {
+            let rollNo = [1,2,3,4,5]
+            resolve(rollNo);
+            reject(`error while fetching data`)
+        }, 2000);
+    })
+
+
+
+promiseObj.then((rollNo)=>{
+    console.log(`Success: ${rollNo}`);
+}).catch((e)=>{
+    console.log(`Error: ${e}`);
+})
+
